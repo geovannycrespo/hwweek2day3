eaa8f11cf5788f1d57aa700c01683bc2017147ee
Geovanny Crespo <geovannycrespo@yahoo.comgit config --global user.email geovannycrespo@yahoo.comgit user.emailgit remote add origin https://github.com/geovannycrespo/codingtemple-december-gittest.gitgit push -u origin mastergit push -u origin master>
2019-12-11 23:37:44 -0600

    homeworkday3

---
+++ hwday3.py
@@ -0,0 +1,9 @@
+
+def calc_circle(radius):
+    circle_area = math.pi * radius ** 2
+    import math 
+    return circle_area
+
+def calc_rectangle(width, length):
+    rect_area = lenght * width
+    return rect_area
